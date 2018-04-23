5614ee6221ec6c4aabd37e6b3a4b76725c7c81b6
Christopher Fung <12625291@student.uts.edu.au>
2018-04-23 19:29:16 +1000

    Super basic limited visibility implementation (large overlay that moves with player), toggles on and off based on player/enemy turn

--- Assets/Scripts/PlayerProgramController.cs
+++ Assets/Scripts/PlayerProgramController.cs
@@ -131,6 +131,7 @@ public class PlayerProgramController : MonoBehaviour {
     }
 
     IEnumerator ExecuteActionList() {
+        DarkRoomController._instance.SetMoving(true);
         foreach (String actionFunc in actions) {
             Debug.Log(actionFunc);
             yield return StartCoroutine(actionFunc);
@@ -138,6 +139,7 @@ public class PlayerProgramController : MonoBehaviour {
         actions.Clear();
         currNumOfActions = 0;
         UpdateActionText();
+        DarkRoomController._instance.SetMoving(false);
         TurnController._instance.EnemyTurn();
     }
 
