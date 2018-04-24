02b6c2f58a58565ce7e3d1a997ca4aac6b6fed82
DESKTOP-N8UA4CN\User <dangtuanlinh1992@gmail.com>
2018-04-25 01:31:47 +1000

    As a Player, I want there to be melee, ranged enemy and can be damaged if being fought by enemies so that I am more challenging to win game

--- Assets/Scripts/Scene2/Bullet.cs
+++ Assets/Scripts/Scene2/Bullet.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
-
+    public int amount;
     private void Update()
     {
         Destroy(gameObject, 1);
     }
-
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            target.gameObject.GetComponent<Player>().damaged(amount);
+        }
+    }
 }
