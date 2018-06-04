6199f1068fd009403e9689e681ebe847cad847d7
Christopher Fung <12625291@student.uts.edu.au>
2018-06-04 17:17:05 +1000

    As a Player I want background music so that more of an atmosphere is added to the game
    Added in the bgm, need to add in volume control

---
+++ Assets/BGMController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMController : MonoBehaviour {
+    public static BGMController _instance;
+    public AudioClip[] bgm;
+    public AudioSource audioSrc;
+
+    private void Awake() {
+        if (_instance != null && _instance != this) {
+            Destroy(gameObject);
+        } else {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    void OnEnable() {
+        SceneManager.sceneLoaded += OnLevelLoaded;
+    }
+
+    void OnDisable() {
+        SceneManager.sceneLoaded -= OnLevelLoaded;
+    }
+
+    void OnLevelLoaded(Scene scene, LoadSceneMode mode) {
+        if (scene.name == "Title" || scene.name == "Hub") {
+            audioSrc.clip = bgm[0];
+        } else if (scene.buildIndex >= 1 && scene.buildIndex <= 5) {
+            audioSrc.clip = bgm[1];
+        } else if (scene.name == "Combat") {
+            audioSrc.clip = bgm[2];
+        }
+
+        audioSrc.Play();
+    }
+}
